63bb834145d31d5bd6996472e2aed74493873f14
Evgenii <sukhov.es@yandex.ru>
2026-01-11 16:36:35 +0300

    Initial Commit

---
+++ DopamineOS beta build/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DopamineOS beta build
+//
+//  Created by Evgenii Sukhov on 11.01.2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ DopamineOS beta buildTests/DopamineOS_beta_buildTests.swift
@@ -0,0 +1,17 @@
+//
+//  DopamineOS_beta_buildTests.swift
+//  DopamineOS beta buildTests
+//
+//  Created by Evgenii Sukhov on 11.01.2026.
+//
+
+import Testing
+@testable import DopamineOS_beta_build
+
+struct DopamineOS_beta_buildTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
